ad08289be9339ce44792d40d8f2c314aec52aaf8
yin1999 <15844309+yin1999@users.noreply.github.com>
2021-05-03 10:54:35 +0800

    feat: using post method to get subscribe details

--- handler.go
+++ handler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 	"os"
 
@@ -9,6 +10,16 @@ import (
 	"firebase.google.com/go/messaging"
 	"google.golang.org/api/option"
 )
+
+type payload struct {
+	Method string `json:"method"`
+	Token  string `json:"token"`
+}
+
+type response struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
 
 func newClient() (client *messaging.Client, err error) {
 	ctx := context.Background()
@@ -23,60 +34,89 @@ func newClient() (client *messaging.Client, err error) {
 }
 
 func ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	param := r.URL.Query()
 	if accessOrigin != "" {
 		w.Header().Add("Access-Control-Allow-Origin", accessOrigin)
 	}
-	if _, ok := param["subscribe"]; ok {
-		subscribe(w, r)
-	} else if _, ok = param["unsubscribe"]; ok {
-		unsubscribe(w, r)
+	if r.Method == http.MethodOptions { // CORS
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Add("Access-Control-Allow-Methods", http.MethodPost)
+		w.Header().Add("Access-Control-Max-Age", "86400")
+		w.Header().Add("Content-Length", "0")
+		return
+	}
+	if r.Method == http.MethodPost {
+		p := &payload{}
+
+		if err := json.NewDecoder(r.Body).Decode(p); err != nil {
+			writeJSON(w, &response{
+				Status:  "error",
+				Message: "cannot decode request body",
+			})
+			return
+		}
+		switch p.Method {
+		case "unsubscribe", "subscribe":
+			subscribeManage(w, p)
+		default:
+			writeJSON(w, &response{
+				Status:  "error",
+				Message: "unsupport method",
+			})
+		}
 	} else {
 		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("resources not found"))
+		writeJSON(w, &response{
+			Status:  "error",
+			Message: "resources not found",
+		})
 	}
 }
 
-func subscribe(w http.ResponseWriter, r *http.Request) {
-	token := r.URL.Query().Get("token")
-	if token == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("param token is nil"))
+func writeJSON(w http.ResponseWriter, data interface{}) {
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	encoder.Encode(data)
+}
+
+func subscribeManage(w http.ResponseWriter, p *payload) {
+	if p.Token == "" {
+		writeJSON(w, &response{
+			Status:  "error",
+			Message: "param token is nil",
+		})
 		return
 	}
 	client, err := newClient()
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("can't create messaging client"))
+		writeJSON(w, &response{
+			Status:  "error",
+			Message: "can't create messaging client",
+		})
 		return
 	}
-	_, err = client.SubscribeToTopic(context.Background(), []string{token}, "all")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("can't subscribe topic"))
-		return
-	}
-	w.Write([]byte("subscribe successfully"))
-}
 
-func unsubscribe(w http.ResponseWriter, r *http.Request) {
-	token := r.URL.Query().Get("token")
-	if token == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("param token is nil"))
+	switch p.Method {
+	case "subscribe":
+		_, err = client.SubscribeToTopic(context.Background(), []string{p.Token}, "all")
+	case "unsubscribe":
+		_, err = client.UnsubscribeFromTopic(context.Background(), []string{p.Token}, "all")
+	default:
+		writeJSON(w, &response{
+			Status:  "error",
+			Message: "unsupport method",
+		})
 		return
 	}
-	client, err := newClient()
+
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("can't create messaging client"))
+		writeJSON(w, &response{
+			Status:  "error",
+			Message: p.Method + " failed",
+		})
 		return
 	}
-	_, err = client.UnsubscribeFromTopic(context.Background(), []string{token}, "all")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("can't unsubscribe topic"))
-		return
-	}
-	w.Write([]byte("unsubscribe successfully"))
+	writeJSON(w, &response{
+		Status:  "ok",
+		Message: p.Method + " successfully",
+	})
 }
